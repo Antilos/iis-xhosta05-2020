1a4d6b86d687563461cc5bac48f85c3ee6186b15
Jakub Kocalka <jak.kocalka@gmail.com>
2020-10-30 20:28:20 +0100

    config change

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
+    DEVELOPMENT = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
