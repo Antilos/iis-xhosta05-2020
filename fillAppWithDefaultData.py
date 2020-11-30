5557ae4a57f560ade93799261532b0d5255083b4
Jakub Kocalka <jak.kocalka@gmail.com>
2020-11-30 16:08:07 +0100

    changed some default users

--- fillAppWithDefaultData.py
+++ fillAppWithDefaultData.py
@@ -4,16 +4,17 @@ import subprocess
 Users = [
     #[name, password, profile_vis]
     ["admin", "admin", "0"],
-    ["test1", "test", "0"],
-    ["test2", "test", "1"],
-    ["test3", "test", "3"]
+    ["oliver", "test", "0"],
+    ["eva", "test", "1"],
+    ["liam", "test", "3"]
+    ["ema", "test", "3"]
 ]
 
 Groups = [
     #[name, visibility, isOpen, ownerName]
-    ["publicGroup", "0", "True", "admin"],
-    ["registeredGroup", "1", "True", "admin"],
-    ["privateGroup", "3", "True", "admin"],
+    ["PublicGroup", "0", "True", "admin"],
+    ["OnlyRegisteredGroup", "1", "True", "admin"],
+    ["PrivateGroup", "3", "False", "admin"],
 ]
 
 Threads = [
